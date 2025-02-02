88eb312726990eb0dd21a0289790f308795ead61
Deven10199 <Deven10199@gmail.com>
2025-02-01 21:17:36 -0500

    Commit 1
    Set up HTML file
    -added ids rock,paper,scissors
    Set up Javascript file
    Set up CSS file

---
+++ js.js
@@ -0,0 +1,8 @@
+const rock = document.getElementById("rock").innerHTML;
+const paper = document.getElementById("paper").innerHTML;
+const scissors = document.getElementById("scissors").innerHTML;
+
+function printRock(){
+
+}
+
